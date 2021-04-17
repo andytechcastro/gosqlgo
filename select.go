67855274928d9620b851454902f7c5e40f2c6b1c
Andrés Castro <andy.tech.castro@gmail.com>
2021-04-17 21:35:55 +0200

    Initial Commit

---
+++ select.go
@@ -0,0 +1,174 @@
+package gosqlgo
+
+import "strings"
+
+type Table struct {
+	table  string
+	fields []string
+	where  map[string]string
+	order  []string
+	joins  map[string]joinTable
+	group  []string
+	offset int
+	limit  int
+}
+
+type joinTable struct {
+	table    string
+	on       string
+	fields   []string
+	typeJoin int
+}
+
+func AddTable(table string) *Table {
+	p := Table{table: table}
+	p.fields = nil
+	p.group = nil
+	p.joins = make(map[string]joinTable)
+	return &p
+}
+
+func (r *Table) InnerJoin() int {
+	return 1
+}
+
+func (r *Table) LeftJoin() int {
+	return 2
+}
+
+func (r *Table) RightJoin() int {
+	return 3
+}
+
+func (r *Table) OuterJoin() int {
+	return 4
+}
+
+func (r *Table) Select() string {
+	query := "SELECT * FROM " + r.table
+	return query
+}
+
+func (r *Table) Insert(newdata map[string]string) string {
+	fields := make([]string, 0, len(newdata))
+	data := make([]string, 0, len(newdata))
+	if len(newdata) != 0 {
+		for key, value := range newdata {
+			fields = append(fields, key)
+			data = append(data, value)
+		}
+	}
+	insert := "INSERT INTO " + r.table + " (" + strings.Join(fields, ", ") + ") VALUES('" + strings.Join(data, "', '") + "');"
+	return insert
+}
+
+func (r *Table) Update(changes map[string]string, conditions map[string]string) string {
+	values := make([]string, 0, len(changes))
+	if len(changes) != 0 {
+		for field, data := range changes {
+			values = append(values, field+" = '"+data+"'")
+		}
+	}
+	where := make([]string, 0, len(conditions))
+	if len(conditions) != 0 {
+		for field, data := range conditions {
+			where = append(where, field+" = '"+data+"'")
+		}
+	}
+	update := "UPDATE " + r.table + " SET " + strings.Join(values, ", ") + " WHERE " + strings.Join(where, " AND ")
+	return update
+}
+
+func (r *Table) Delete(conditions map[string]string) string {
+	where := make([]string, 0, len(conditions))
+	if len(conditions) != 0 {
+		for field, data := range conditions {
+			where = append(where, field+" = '"+data+"'")
+		}
+	}
+	delete := "DELETE FROM " + r.table + " WHERE " + strings.Join(where, " AND ")
+	return delete
+}
+
+func (r *Table) Columns(fields []string) {
+	r.fields = fields
+}
+
+func (r *Table) Expression(field string) {
+	r.fields = append(r.fields, field)
+}
+
+func (r *Table) Where(where map[string]string) {
+	r.where = where
+}
+
+func (r *Table) Order(order []string) {
+	r.order = order
+}
+
+func (r *Table) Group(group []string) {
+	r.group = group
+}
+
+func (r *Table) Limit(offset int, limit int) {
+	r.offset = offset
+	r.limit = limit
+}
+
+func (r *Table) getQuery() string {
+	join := ""
+	fieldsJoin := ""
+	if len(r.joins) != 0 {
+		for njoin, data := range r.joins {
+			inner := " "
+			if data.typeJoin == 2 {
+				inner += "LEFT"
+			} else if data.typeJoin == 3 {
+				inner += "RIGHT"
+			} else if data.typeJoin == 4 {
+				inner += "OUTER"
+			} else {
+				inner += "INNER"
+			}
+			join += inner + " JOIN " + njoin + " ON " + data.on
+			coma := ""
+			if r.fields != nil {
+				coma += ", "
+			}
+			if data.fields != nil {
+				fieldsJoin += coma + strings.Join(data.fields, ", ")
+			}
+		}
+	}
+	fields := ""
+	if len(r.fields) != 0 {
+		fields += strings.Join(r.fields, ", ")
+	} else {
+		if fieldsJoin == "" {
+			fields += "*"
+		}
+	}
+	whereString := ""
+	if len(r.where) != 0 {
+		where := make([]string, 0, len(r.where))
+		for field, data := range r.where {
+			where = append(where, field+" = '"+data+"'")
+		}
+		whereString = " WHERE " + strings.Join(where, " AND ")
+	}
+	order := ""
+	if len(r.order) != 0 {
+		order += " ORDER BY " + strings.Join(r.order, ", ")
+	}
+	group := ""
+	if r.group != nil {
+		group += " GROUP BY " + strings.Join(r.group, ", ")
+	}
+	query := "SELECT " + fields + fieldsJoin + " FROM " + r.table + join + whereString + order + group
+	return query
+}
+
+func (r *Table) Join(table string, on string, fields []string, typeJoin int) {
+	join := joinTable{table: table, on: on, fields: fields, typeJoin: typeJoin}
+	r.joins[table] = join
+}
---
+++ select_test.go
@@ -0,0 +1,90 @@
+package gosqlgo
+
+import "testing"
+
+func TestNormalSelect(t *testing.T) {
+	want := "SELECT name, lastname FROM users"
+	users := AddTable("users")
+	users.Columns([]string{"name", "lastname"})
+	if got := users.getQuery(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestWhereSelect(t *testing.T) {
+	want := "SELECT name, lastname FROM users WHERE idUser = '1' AND name = 'Andres'"
+	users := AddTable("users")
+	users.Columns([]string{"name", "lastname"})
+	users.Where(map[string]string{"idUser": "1", "name": "Andres"})
+	if got := users.getQuery(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestOrder(t *testing.T) {
+	want := "SELECT * FROM users ORDER BY firstname, lastname"
+	users := AddTable("users")
+	users.Order([]string{"firstname", "lastname"})
+	if got := users.getQuery(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestGroup(t *testing.T) {
+	want := "SELECT * FROM users GROUP BY firstname"
+	users := AddTable("users")
+	users.Group([]string{"firstname"})
+	if got := users.getQuery(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestFetchAll(t *testing.T) {
+	want := "SELECT * FROM users"
+	users := AddTable("users")
+	if got := users.Select(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestJoin(t *testing.T) {
+	want := "SELECT habitaciones, baños FROM users INNER JOIN casa ON users.idCasa = casa.idCasa"
+	users := AddTable("users")
+	users.Join("casa", "users.idCasa = casa.idCasa", []string{"habitaciones", "baños"}, users.InnerJoin())
+	if got := users.getQuery(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestMultiJoin(t *testing.T) {
+	want := "SELECT * FROM users LEFT JOIN casa ON users.idCasa = casa.idCasa RIGHT JOIN oficina ON users.idOficina = oficina.idOficina"
+	users := AddTable("users")
+	users.Join("casa", "users.idCasa = casa.idCasa", nil, users.LeftJoin())
+	users.Join("oficina", "users.idOficina = oficina.idOficina", nil, users.RightJoin())
+	if got := users.getQuery(); got != want {
+		t.Errorf("getQuery() = %q, want %q", got, want)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	want := "INSERT INTO users (firstname, lastname) VALUES('Andres', 'Castro');"
+	user := AddTable("users")
+	if got := user.Insert(map[string]string{"firstname": "Andres", "lastname": "Castro"}); got != want {
+		t.Errorf("Insert() = %q, want %q", got, want)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	want := "DELETE FROM users WHERE idUser = '1' AND firstname = 'Andres'"
+	user := AddTable("users")
+	if got := user.Delete(map[string]string{"idUser": "1", "firstname": "Andres"}); got != want {
+		t.Errorf("Delete() = %q, want %q", got, want)
+	}
+}
+func TestUpdate(t *testing.T) {
+	want := "UPDATE users SET firstname = 'Andres', lastname = 'Castro' WHERE idUser = '1'"
+	user := AddTable("users")
+	if got := user.Update(map[string]string{"firstname": "Andres", "lastname": "Castro"}, map[string]string{"idUser": "1"}); got != want {
+		t.Errorf("Update() = %q, want %q", got, want)
+	}
+}
